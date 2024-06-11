325e3da091a94e49c58965fc4988ba396a1775f9
soyoka <55011525+soy0ka@users.noreply.github.com>
2024-06-11 16:43:41 +0900

    chore: Update Loading component to use CircularProgress instead of image

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { Box } from '@mui/material'
-import Loading from '~/assets/loading.gif'
+import { Box, CircularProgress } from '@mui/material'
 import { wrapError } from '~/components/ErrorBoundary'
 
 export const LoadingImage: React.FC = wrapError(() => {
@@ -8,7 +7,7 @@ export const LoadingImage: React.FC = wrapError(() => {
     <Box
       sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
     >
-      <img src={Loading} alt="loading" />
+      <CircularProgress />
     </Box>
   )
 })
--- src/routes/main/index.tsx
+++ src/routes/main/index.tsx
@@ -4,6 +4,7 @@ import ButtonStyle from '~/styles/button'
 import { Box, Button, Input, useMediaQuery, useTheme } from '@mui/material'
 import { postFetcher } from '~/util'
 import { toast } from 'react-toastify'
+import { LoadingImage } from '~/components/Loading'
 
 export const Main: React.FC = wrapError(() => {
   const tlsRegex = /^https:\/\/tls\.kku\.ac\.kr\/local\/ubdoc\/\?.*$/
@@ -11,12 +12,18 @@ export const Main: React.FC = wrapError(() => {
   const isMobile = useMediaQuery(theme.breakpoints.down('md'))
 
   const [url, setUrl] = React.useState('')
+  const [loading, setLoading] = React.useState(false)
+  const [fileLink, setFileLink] = React.useState('')
 
   const handleSubmint = () => {
     if (!url) return toast.error('URL을 입력해주세요')
     if (!tlsRegex.test(url)) return toast.error('TLS 문서 링크를 입력해주세요')
+    setLoading(true)
     postFetcher('/download', { url }).then((res) => {
-      console.log(res)
+      setLoading(false)
+      if (!res || !res.success) return toast.error('다운로드 실패')
+      toast.success('다운로드 성공')
+      setFileLink(res.body.url)
     })
   }
   return (
@@ -49,6 +56,26 @@ export const Main: React.FC = wrapError(() => {
       >
         다운로드
       </Button>
+      {fileLink && (
+        <a href={`https://omicron.onde.us/api/file${fileLink}`} download>
+          <Button
+            variant="contained"
+            sx={{ ...ButtonStyle.maxed, marginTop: '10px' }}
+            onClick={() => setFileLink('')}
+          >
+            파일 다운로드
+          </Button>
+        </a>
+      )}
+      {loading && (
+        <Box
+          sx={{
+            marginTop: '10px',
+          }}
+        >
+          <LoadingImage />
+        </Box>
+      )}
     </Box>
   )
 })
